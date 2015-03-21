bd5cc6fe8d011d7e87a984a3b08efcb727ddecaf
amandaghassaei <amandaghassaei@gmail.com>
2015-03-21 17:18:00 -0400

    sample linkage

--- js/main.js
+++ js/main.js
@@ -23,13 +23,19 @@ $(function(){
     new Ribbon({model:globals.appState});
 
     //do stuff
-    var hinge1 = globals.linkage.addHingeAtPosition(new THREE.Vector2(1,5));
-    var hinge2 = globals.linkage.addHingeAtPosition(new THREE.Vector2(-3,10));
-    var link = globals.linkage.link(hinge1, hinge2);
-    globals.linkage.addDriveCrank(hinge1, hinge2, link);
-    var hinge3 = globals.linkage.addHingeAtPosition(new THREE.Vector2(5,1));
+    var hinge1 = globals.linkage.addHingeAtPosition(new THREE.Vector2(0,20));
+    var hinge2 = globals.linkage.addHingeAtPosition(new THREE.Vector2(0,-20));
+    var hinge3 = globals.linkage.addHingeAtPosition(new THREE.Vector2(-10,0));
+    var hinge4 = globals.linkage.addHingeAtPosition(new THREE.Vector2(14,0));
+    hinge4.setStatic(true);
+    var hinge5 = globals.linkage.addHingeAtPosition(new THREE.Vector2(-20,0));
+
+    globals.linkage.link(hinge1, hinge3);
     globals.linkage.link(hinge3, hinge2);
-    //hinge3.setStatic(true);
+    globals.linkage.link(hinge2, hinge4);
+    globals.linkage.link(hinge4, hinge1);
+    var link35 = globals.linkage.link(hinge3, hinge5);
+    globals.linkage.addDriveCrank(hinge5, hinge3, link35);
 
     //threeJS View
     new ThreeView({model:globals.three});
