c691cb624e52079655ae2bd20d3247c0ad4e870c
amandaghassaei <amandaghassaei@gmail.com>
2015-03-22 16:44:57 -0400

    more script edits

--- js/menus/ScriptMenuView.js
+++ js/menus/ScriptMenuView.js
@@ -15,10 +15,11 @@ ScriptMenuView = Backbone.View.extend({
 
     initialize: function(){
 
-        _.bindAll(this, "render");
+        _.bindAll(this, "render", "_handleKeyStroke");
 
         //bind events
         this.listenTo(globals.appState, "change:currentTab", this.render);
+        $(document).bind('keydown', {}, this._handleKeyStroke);
 
         globals.script = function(){
     var hinge1 = globals.linkage.addHingeAtPosition({x:0,y:20});
@@ -36,17 +37,30 @@ ScriptMenuView = Backbone.View.extend({
 };
     },
 
+    _handleKeyStroke: function(e){
+        if (e.keyCode == 82 && this.model.get("currentTab") == "script"){
+            if (e.shiftKey || !e.metaKey) return;
+            e.stopPropagation();
+            this._runScript(e);
+        }
+    },
+
+    _syncScript: function(){
+        eval("globals.script =" + this.codeMirror.getValue());
+    },
+
     _runScript: function(e){
         e.preventDefault();
         globals.linkage.clearAll();
-        eval("globals.script =" + this.codeMirror.getValue());
+        this._syncScript();
         globals.script();
         globals.appState.set("isAnimating", true);
     },
 
     _saveScript: function(e){
         e.preventDefault();
-        globals.appState.saveFile(this.script, "linkageScript", ".js");
+        this._syncScript();
+        globals.appState.saveFile(globals.script, "linkageScript", ".js");
     },
 
     _loadScript: function(e){
@@ -76,7 +90,7 @@ ScriptMenuView = Backbone.View.extend({
 
     template: _.template('\
             <div class="col-sm-4"><a href="#" id="loadScript" class=" btn btn-lg btn-block btn-default">Load Script</a></div>\
-            <div class="col-sm-4"><a href="#" id="runScript" class=" btn btn-lg btn-block btn-default">Run Script</a></div>\
+            <div class="col-sm-4"><a href="#" id="runScript" class=" btn btn-lg btn-block btn-default">Run Script&nbsp&nbsp&nbsp(CTRL/&#8984; + R)</a></div>\
             <div class="col-sm-4"><a href="#" id="saveScript" class=" btn btn-lg btn-block btn-default">Save Script</a></div><br/><br/>\
             <textarea id="scriptEditor"><%= script %></textarea><br/>\
         ')
