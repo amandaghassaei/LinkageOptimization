1ed385ae383d10b9311a555a8509e2917e0d59e8
jming <jming@college.harvard.edu>
2015-04-13 13:41:16 -0400

    create curve file

---
+++ js/models/Curve.js
@@ -0,0 +1,15 @@
+
+function fitCurve(points) {
+
+	// return curve function parameters
+}
+
+function compareCurve(curve_in, curve_test) {
+
+	// potential approaches:
+		// - try to fit two curves and see how similar they are
+		// - try to take the fitted curve 
+
+	// return some score based on how well the curve fits
+
+}
