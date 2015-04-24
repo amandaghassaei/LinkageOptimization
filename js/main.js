f437a0fc323fa39fb2e2f81c990df10d470c223a
Amanda Ghassaei <amandaghassaei@gmail.com>
2015-04-24 16:01:05 -0400

    detect domain

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@ $(function(){
     //init web workers
 //    window.workers = persistentWorkers(8);
 
+    console.log(document.domain);
 //    $(window).bind('beforeunload', function(){
 //      return 'Are you sure you want to leave?';
 //    });
