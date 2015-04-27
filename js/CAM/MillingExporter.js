b51c249ed6ce40fcb802b42bd38d357aa9776ed0
Amanda Ghassaei <amandaghassaei@gmail.com>
2015-04-27 18:44:35 -0400

    make outline around path with arb geometry and corner rad

--- js/CAM/MillingExporter.js
+++ js/CAM/MillingExporter.js
@@ -40,14 +40,53 @@ MillingExporter = Backbone.Model.extend({
         });
         var tri012 = new THREE.Line(tri012Geometry);
         globals.three.sceneAdd(tri012);
-        _.each([0,1,2], function(num){
-
-        });
 
 
+        globals.three.sceneAdd(this._makeOutlinePath(json.hinges, [0, 1, 2, 0], this.get("linkWidth")/2.0));
 
 
     },
+
+
+    _makeOutlinePath: function(hinges, indices, offset){
+        var geometry = new THREE.Geometry();
+        var vertices = [];
+        for (var i=0;i<indices.length-1;i++){
+            var theta = Math.PI/2 + this._getAngle(hinges[indices[i]].position, hinges[indices[i+1]].position);
+            var positionOffset = {x:offset*Math.cos(theta), y:offset*Math.sin(theta)};
+            vertices.push(new THREE.Vector3(hinges[indices[i]].position.x+positionOffset.x, hinges[indices[i]].position.y+positionOffset.y, 0));
+            vertices.push(new THREE.Vector3(hinges[indices[i+1]].position.x+positionOffset.x, hinges[indices[i+1]].position.y+positionOffset.y, 0));
+
+        }
+        for (var i=1;i<vertices.length;i+=2){
+            geometry.vertices.push(vertices[i-1]);
+            geometry.vertices.push(vertices[i]);
+            var centerPoint = hinges[indices[Math.floor(i/2)+1]].position;
+            var startingTheta = this._getAngle(centerPoint, vertices[i]);
+            var nextVertex = vertices[0];
+            if (i+1<vertices.length-1) nextVertex = vertices[i+1];
+            var endingTheta = this._getAngle(centerPoint, nextVertex);
+            var cornerRadius = this._makeArc(vertices[i], startingTheta, endingTheta, centerPoint, offset);
+            geometry.vertices = geometry.vertices.concat(cornerRadius);
+        }
+        geometry.vertices.push(geometry.vertices[0]);//close loop
+        return new THREE.Line(geometry);
+    },
+
+    _makeArc: function(startingPoint, startingTheta, endingTheta, centerPoint, radius){
+        var vertices = [];
+        if (endingTheta<startingTheta) endingTheta += Math.PI*2;
+        var thetaIncr = (endingTheta-startingTheta)*Math.PI*2/100;
+        for (var i=startingTheta+thetaIncr;i<endingTheta;i+=thetaIncr){
+            vertices.push(new THREE.Vector3(-radius*Math.cos(i)+centerPoint.x, -radius*Math.sin(i)+centerPoint.y));
+        }
+        return vertices;
+    },
+
+    _getAngle: function(point1, point2){
+        return Math.atan2(point1.y-point2.y, point1.x-point2.x);
+    },
+
 
     drawToScreen: function(paths){
 
