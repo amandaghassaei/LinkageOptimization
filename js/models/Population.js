376a7ef4ca9f2bd38ef62306e4a9c4004d944cb2
jming <jming@college.harvard.edu>
2015-05-03 14:25:44 -0400

    sample target paths

--- js/models/Population.js
+++ js/models/Population.js
@@ -17,9 +17,16 @@ Population.prototype._initFirstGeneration = function(archetype){
     if (archetype === undefined){
         archetype = new Linkage();
 
-        var hinge1 = archetype.addHingeAtPosition({x:15,y:30});
-        var hinge2 = archetype.addHingeAtPosition({x:0,y:40});
-        var hinge3 = archetype.addHingeAtPosition({x:-10,y:0});
+        // var hinge1 = archetype.addHingeAtPosition({x:15,y:30});
+        // var hinge2 = archetype.addHingeAtPosition({x:0,y:40});
+        // var hinge3 = archetype.addHingeAtPosition({x:-10,y:0});
+
+        var hinge1 = archetype.addHingeAtPosition({x: (Math.random()-0.5)*30, y: (Math.random()-0.5)*30});
+        var hinge2 = archetype.addHingeAtPosition({x: (Math.random()-0.5)*30, y: (Math.random()-0.5)*30});
+        var hinge3 = archetype.addHingeAtPosition({x: (Math.random()-0.5)*30, y: (Math.random()-0.5)*30});
+
+        console.log(hinge1, hinge2, hinge3);
+
         var hinge4 = archetype.addHingeAtPosition({x:14,y:2}).setStatic(true);
         var hinge5 = archetype.addHingeAtPosition({x:-20,y:-2});
 
